567b8ffc4b90b899f4a341dca8ded283bae98cce
LuiFeAn <luisfernandogvv@gmail.com>
2024-09-14 20:05:22 -0300

    refactor:  adjusting interface

--- src/domain/assistant/IAssistantMemory.java
+++ src/domain/assistant/IAssistantMemory.java
@@ -3,7 +3,6 @@ package domain.assistant;
 import domain.user.Client;
 
 public interface IAssistantMemory {
-    public void register(Client client);
-    public void unregister(Client client);
-    public Client find(String email);
+    void register(Client client);
+    Client find(String email);
 }
