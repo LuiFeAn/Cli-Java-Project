47551fb124f0783c57ec7aadeff5ee7efb668af6
LuiFeAn <luisfernandogvv@gmail.com>
2024-09-15 11:23:15 -0300

    fix:  adjusting regex pattern

--- src/domain/user/Username.java
+++ src/domain/user/Username.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class Username {
 
-    private static final String USERNAME_REGEX = "^[a-zA-Z0-9_]+$";
+    private static final String USERNAME_REGEX = "^[a-zA-Z0-9_ ]+$";
     private static final Pattern USERNAME_PATTERN = Pattern.compile(USERNAME_REGEX);
 
     private String _value;
@@ -25,7 +25,7 @@ public class Username {
         }
 
         if (!USERNAME_PATTERN.matcher(this._value).matches()) {
-            throw new InvalidParameterException("Invalid Username: Username cannot contain special characters");
+            throw new InvalidParameterException("Invalid Username: Username contains special characters");
         }
     }
 }
