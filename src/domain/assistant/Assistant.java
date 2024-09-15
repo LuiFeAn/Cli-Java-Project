36bd4602e7ea9ffdd9b0612d60659be49a5d1344
LuiFeAn <luisfernandogvv@gmail.com>
2024-09-15 11:23:41 -0300

    fix: adjusting exception

--- src/domain/assistant/Assistant.java
+++ src/domain/assistant/Assistant.java
@@ -49,6 +49,7 @@ public class Assistant {
 
            }
        }catch(Exception e){
+           System.out.println(e.getMessage());
            System.out.println("Não foi possível registra-lo! Os parâmetros fornecidos são inválidos");
        }
 
